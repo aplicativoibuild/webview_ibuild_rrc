86fa0e24016ca50aee5633466849f73a8d40ab66
aplicativoibuild <aplicativoibuild@gmail.com>
2023-11-24 11:01:00 -0300

    atualizacao24112023

---
+++ novoapp/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+import 'src/menu.dart'; // ADD
+import 'src/navigation_controls.dart';
+import 'src/web_view_stack.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: WebViewApp(),
+    ),
+  );
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..loadRequest(
+        Uri.parse('https://www.ibuildapp.com.br'),
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('iBuild RRC'),
+        actions: [
+          NavigationControls(controller: controller),
+          Menu(controller: controller), // ADD
+        ],
+      ),
+      body: WebViewStack(controller: controller),
+    );
+  }
+}
